40914f401e9b7d042536e7ff4667fdb4b026b03f
Daniel Hilst <danielhilst@gmail.com>
2024-09-10 10:40:49 -0300

    Improve OpenUrlLn

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,10 +1,12 @@
-local function find_link(text)
-  return text:match '(https?://[a-zA-Z0-9_./]*)'
+-- "https://rustjobs.dev/featured-jobs/CoW-DAO-Web3-Backend-Engineer-Rust-Or8QRUSqChHp51ugFvX2",
+function find_link(text)
+  return text:match '(https?://[a-zA-Z0-9_.%-/]*)'
 end
 
-function open_url_ln()
+local function open_url_ln()
   local link = find_link(vim.api.nvim_get_current_line())
   if link ~= nil then
+    vim.cmd(("echo '%s'"):format(link))
     vim.cmd(string.format('exec "silent !firefox %s"', link))
   else
     vim.cmd "echo 'No link found'"
